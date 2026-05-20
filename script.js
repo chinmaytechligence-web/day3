ec97a36321e7f1259dc86276fc4671e4ac8b7f07
ChinmayBG <chinmay.techligence@gmail.com>
2026-05-20 20:45:53 +0530

    added columns and their id. Also added drag and drop functionality

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const repoInput = document.getElementById("repo-input");
 const searchbtn = document.getElementById("search-btn");
-const openCard = document.getElementById("open-Cards");
-const reviewCard = document.getElementById("review-Cards");
-const closedCard = document.getElementById("closed-Cards");
+const openCards = document.getElementById("open-Cards");
+const reviewCards = document.getElementById("review-Cards");
+const closedCards = document.getElementById("closed-Cards");
 const errorMessage = document.getElementById("errorMessage");
+const repoInfo = document.getElementById("repoInfo");
+const openColumn =document.getElementById("openColumn");
+const reviewColumn =document.getElementById("reviewColumn");
+const closedColumn = document.getElementById("closedColumn");
 
 searchbtn.addEventListener("click", () => {
-  const userInput = repoInput.ariaValueMax.trim();
+  const userInput = repoInput.value.trim();
 
   if (userInput === "") {
     alert("Please enter valid url or user/reponame");
@@ -37,28 +41,30 @@ async function fetchIssues(userInput) {
       [owner, repo] = userInput.split("/");
 
     
-    const url = (`https://github.com/${owner}/${repo}/issues`);
+    const repoURL =`https://api.github.com/repos/${owner}/${repo}`;
+    const issuesURL =`https://api.github.com/repos/${owner}/${repo}/issues`;
 
     // get data
-    const response = await fetch(url);
+    const [repoResponse, issuesResponse] =await Promise.all([fetch(repoURL),fetch(issuesURL)]);
     
     // 404 ERROR
-    if(response.status === 404){
+    if(repoResponse.status === 404){
       throw new Error("Repo not found");
     }
 
     // 403 ERROR
-    if(response.status === 403){
+    if(repoResponse.status === 403){
       throw new Error("Too many requests. Try later.");
     }
 
     // OTHER ERRORS
-    if(!response.ok){
+    if(!repoResponse.ok||!issuesResponse.ok){
       throw new Error("Something went wrong");
     }
 
     //get json response
-    const issues = await response.json();
+    const repoData = await repoResponse.json();
+    const issues = await issuesResponse.json();
 
     //clearing previous cards 
     openCard.innerHTML = "";
@@ -89,6 +95,7 @@ async function fetchIssues(userInput) {
     
   } catch (error) {
     removeLoading();
+    showRepoInfo(repoData);
 
     if(error.message === "Failed to fetch"){
     errorMessage.textContent ="Check your internet connection";}
@@ -99,6 +106,15 @@ async function fetchIssues(userInput) {
 
 }
 
+function showRepoInfo(repo){
+  repoInfo.innerHTML = `
+    <h2>${repo.full_name}</h2>
+    <p>${repo.description || "No description"}</p>
+    <br>
+    <p>Stars: ${repo.stargazers_count}</p>
+    <p>🍴 Forks: ${repo.forks_count}</p>`;
+}
+
 function createCard(issue){
 
   // create div
@@ -106,6 +122,8 @@ function createCard(issue){
 
   // add class
   card.classList.add("card");
+
+  card.setAttribute("draggable", true);
 
   // labels html
   let labelsHTML = "";
@@ -121,9 +139,41 @@ function createCard(issue){
     <div class="labels">${labelsHTML}</div>
     <p>Comments: ${issue.comments}</p>`;
 
+  card.addEventListener("dragstart", () => {
+    card.classList.add("dragging");
+  });
+
+  card.addEventListener("dragend", () => {
+    card.classList.remove("dragging");
+  });
+
   return card;
 
 }
+const columns = [
+  openColumn,
+  reviewColumn,
+  closedColumn
+];
+
+
+
+// LOOP THROUGH COLUMNS
+columns.forEach(column => {
+  // ALLOW DROP
+  column.addEventListener("dragover", (e) => {e.preventDefault();});
+
+  // HANDLE DROP
+  column.addEventListener("drop", () => {
+    // currently dragged card
+    const draggingCard = document.querySelector(".dragging");
+    
+    // append into cards container
+    column.querySelector(".cards").appendChild(draggingCard);
+  
+  });
+});
+
 
 // SHOW LOADING
 function showLoading(){
